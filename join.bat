dbbe933939dee33716cbc8d9a60f683a1d129c31
Aereli <aerelioyanadel@gmail.com>
2020-01-17 17:15:20 -0500

    setup joinner for misc files in path

---
+++ join.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo wscript.quit MsgBox ("Continue?", 4, "Please choose") > yesno.vbs
+wscript //nologo yesno.vbs
+set value=%errorlevel%
+del yesno.vbs
+if "%value%"=="6" TYPE ZIGI* > newfile.txt
+if "%value%"=="7" msg * you canceled!
+
+
+exit >nul
+
+
