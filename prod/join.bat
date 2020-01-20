4a5ec442bf06c892f256a0611880a952bc88a197
Aereli <aerelioyanadel@gmail.com>
2020-01-20 12:38:43 -0500

    move files around

--- prod/join.bat
+++ prod/join.bat
@@ -4,7 +4,7 @@ echo wscript.quit MsgBox ("Continue?", 4, "Please choose") > yesno.vbs
 wscript //nologo yesno.vbs
 set value=%errorlevel%
 del yesno.vbs
-if "%value%"=="6" TYPE ZIGI* > newfile.txt
+if "%value%"=="6" TYPE ZIGI* > BrandedOnline.txt
 if "%value%"=="7" msg * you canceled!
 
 
